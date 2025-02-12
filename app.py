2fcff5ef55057fef922c55b65850cb7aceeed54e
AkiraWin <frog7bar@gmail.com>
2025-02-12 23:18:44 +0900

    no mongo

--- app.py
+++ app.py
@@ -1,23 +1,8 @@
-from flask import Flask, render_template, redirect, url_for
-from pymongo import MongoClient
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-uri = "mongodb+srv://icyowl:moon997sun@cluster0.uqwy3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-client = MongoClient(uri)
-db = client['test_db']
-greet = db.greet
-if not greet.count_documents({}):
-    greet.insert_one({'name': 'alice'})
-
-@app.route('/greet')
-def index():
-    cursor = greet.find()
-    names = list(cursor)
-    return render_template('hello.html', names=names)
-
-@app.route('/greet/<name>')
+@app.route('/')
+@app.route('/<name>')
 def hello(name=None):
-    greet.insert_one({'name': name})
-    return redirect(url_for('index'))
+    return render_template('hello.html', name=name)
