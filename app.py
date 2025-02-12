f39e1f7d46705f903d2ce9e2c21278daf2c9a942
AkiraWin <frog7bar@gmail.com>
2025-02-13 00:00:55 +0900

    add atlas ping test

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
 
 app = Flask(__name__)
+
+uri = "mongodb+srv://icyowl:moon997sun@cluster0.uqwy3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client = MongoClient(uri)
 
 @app.route('/')
 @app.route('/<name>')
 def hello(name=None):
-    return render_template('hello.html', name=name)
+    res = None
+    try:
+        client.admin.command('ping')
+        res = 'You successfully connected to MongoDB!'
+    except Exception as e:
+        res = e
+    return render_template('hello.html', name=name, res=res)
