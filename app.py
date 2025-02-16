1aa724797bdada3b90f0fabd17dfe3af1ea9b0b0
AkiraWin <frog7bar@gmail.com>
2025-02-17 01:01:21 +0900

    add login

--- app.py
+++ app.py
@@ -1,19 +1,56 @@
-from flask import Flask, render_template
-from pymongo import MongoClient
-
+import json
+from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from werkzeug.security import check_password_hash
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
 
 app = Flask(__name__)
 
-uri = "mongodb+srv://icyowl:moon997sun@cluster0.uqwy3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-client = MongoClient(uri)
+app.secret_key = 'mysecretkey'
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(id=user_id)
+
+class LoginForm(FlaskForm):
+    username = StringField('username')
+    password = PasswordField('password')
 
 @app.route('/')
-@app.route('/<name>')
-def hello(name=None):
-    message = ''
-    try:
-        client.admin.command('ping')
-        message = 'You successfully connected to MongoDB!'
-    except Exception as e:
-        message = e
-    return render_template('hello.html', name=name, message=message)
+@login_required
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    db = json.load(open('users.json', 'r'))
+    if request.method == 'POST':
+        username = form.username.data
+        (id, pw_hash), = [(x['id'], x['pw']) for x in db if x['name'] == username]
+        pw_auth = check_password_hash(pwhash=pw_hash, password=form.password.data)
+        if pw_auth:
+            user = User(id=id)
+            login_user(user, remember=True)
+            return redirect(url_for('index'))
+        msg = 'Your name or password does not match!'
+        return render_template('login.html', form=form, msg=msg)
+    return render_template('login.html', form=form, msg='')
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.debug=True
+    app.run()
---
+++ create_users.py
@@ -0,0 +1,17 @@
+import json
+from werkzeug.security import generate_password_hash
+
+data = [
+    {
+        'id': 1,
+        'name': 'Alice',
+        'pw': 'mockturtle11'
+    }
+]
+
+for d in data:
+    d['pw'] = generate_password_hash(d['pw'])
+
+with open('users.json', 'w') as f:
+    json.dump(data, f, indent=2)
+
